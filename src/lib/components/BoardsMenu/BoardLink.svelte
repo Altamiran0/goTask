b0299534c60dbf6826ee63592bcaed44b81b3cd6
Altamiran0 <altamirano.41avc@gmail.com>
2024-05-12 17:23:16 -0300

    Items del menu de navegacion entre tableros

---
+++ src/lib/components/BoardsMenu/BoardLink.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    export let boardName: String;
+    let boardName_url = boardName.replaceAll(' ', '_');
+</script>
+
+<li><a href="/boards/{ boardName_url }">{ boardName }</a></li>
+
+<style>
+    li {
+        width: 100%;
+        height: 50px;
+        border-radius: 5px;
+        font-size: 14px;
+
+        display: flex;
+        align-items: center;
+        justify-self: center;
+        padding: 0 10px;
+        font-weight: 500;
+        text-overflow: ellipsis;
+    }
+
+    li:hover {
+        background: rgba(0, 0, 0, 0.02);
+    }
+
+    a {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        color: var(--Black_1);
+    }
+</style>
