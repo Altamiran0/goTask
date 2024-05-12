bf1674870d31383dff9fbce2f2776cd8e48c9642
Altamiran0 <altamirano.41avc@gmail.com>
2024-05-12 17:25:06 -0300

    Header de la pagina generica para tableros

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -1,11 +1,12 @@
 <script>
-    import IconMenu2 from '@tabler/icons-svelte/IconMenu2.svelte';
+    import BoardsMenu from './../BoardsMenu/BoardsMenu.svelte';
 </script>
 
+
 <header>    
-    <IconMenu2 stroke={1.5}/>
-    <span>goTask_</span>
-    <h1>tablero 1</h1>
+    <BoardsMenu />
+    <span style="font-weight: 700; font-size: 18px;">goTask_</span>
+    <h1 style="font-weight: 500; font-size: 28px;">tablero 1</h1>
 </header>
 
 <style>
@@ -13,9 +14,8 @@
         width: 100dvw;
         height: 55px;
 
-        background: #ebecee;  
-        box-shadow: 0px 1px 5px 0px rgba(0,0,0,0.5);
-        color: rgb(32, 31, 31); 
+        background: var(--White_1);  
+        box-shadow: 0 1px 5px 0 var(--ShadowColor);
 
         display: grid;
         grid-template-columns: 50px 85px 1fr;
