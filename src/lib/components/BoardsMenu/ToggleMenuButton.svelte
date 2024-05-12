70286982c272eef0320ac1ae4612defb63d1a384
Altamiran0 <altamirano.41avc@gmail.com>
2024-05-12 17:24:29 -0300

    boton de activacion del menu

---
+++ src/lib/components/BoardsMenu/ToggleMenuButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import IconMenu2 from '@tabler/icons-svelte/IconMenu2.svelte';
+    import IconX from '@tabler/icons-svelte/IconX.svelte';
+
+    export let statusMenu: boolean;
+    const ChangeState = () => statusMenu = !statusMenu;
+</script>
+
+{#if statusMenu}
+<button style="display: flex" on:click={ ChangeState }> 
+    <IconX stroke={1.5} size={32}/>
+</button>
+{:else}
+<button style="display: flex" on:click={ ChangeState }>
+    <IconMenu2 stroke={1.5}/>
+</button>
+{/if}
